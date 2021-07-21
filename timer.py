f273816614a9abe666ce2b50ef692b13369a4cab
Mickey <mickey.zheng@yahoo.com>
2021-07-21 16:34:53 -0400

    Added count down functionality

--- timer.py
+++ timer.py
@@ -1,10 +1,10 @@
 from tkinter import *
 
+oneSecond = 1000  # 1000ms = 1s
 maxSeconds = 86400  # Max time in seconds allowed
 defaultTime = 60  # Default time in seconds
 numRows = 10
 numCols = 10
-
 widgetList = {}  # Stores all the widgets created (Pixels, label)
 
 
@@ -13,14 +13,14 @@ def createGUI(time):  # Creates the entire GUI
     root.title("Timer")
     root.resizable(False, False)
 
-    # Create GUI design
+    # Create GUI format
     drawTimeLabel(root, time)
     drawGrid(root)
     return root
 
 
 def drawTimeLabel(root, time):  # Creates & positon time label
-    timeLabel = Label(root, font=("Ariel", 20))
+    timeLabel = Label(root, font=("Mono", 20))
     timeLabel.config(text="Time Left: {0}s".format(time))
     timeLabel.grid(row=0, column=0, pady=5, columnspan=int(numRows))
     widgetList["timeLabel"] = timeLabel
@@ -52,10 +52,23 @@ def validateTime(time):  # Checks if time input is valid, defaults to 60 if not
         return defaultTime
 
 
+def updateTime(miliSecLeft):
+    if (miliSecLeft < 0):
+        return
+
+    timeLabel = widgetList["timeLabel"]
+    timeLabel.config(text="Time Left: {0}s".format(int(miliSecLeft/oneSecond)))
+    root.after(oneSecond, updateTime, miliSecLeft - oneSecond)
+
+
 if __name__ == "__main__":
     timeSet = input("Enter timer duration in seconds: ")
     timeSet = validateTime(timeSet)
 
+    # Convert from sec to ms
+    timeSet *= 1000
+
     root = createGUI(timeSet)
+    updateTime(timeSet)
 
     root.mainloop()
